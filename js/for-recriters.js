3a42c8c14d9fa0ae6a2dd991809006c471b29840
prateekch33 <prateekchandra27@outlook.com>
2022-08-13 13:10:31 +0530

    Added Responsiveness in Recruiters Access

--- js/for-recriters.js
+++ js/for-recriters.js
@@ -4,11 +4,15 @@ var demographics = document.querySelector("#demographics");
 var representatives = document.querySelector("#representatives");
 
 var mainContainer = document.querySelectorAll(".main-container");
+var mainContainerMb = document.querySelectorAll(".main-container-mb");
 var buttons = document.querySelectorAll(".button");
 var activeButton = document.querySelector(".active-button");
 
 for (var i = 1; i < mainContainer.length; i++) {
   mainContainer[i].style.display = "none";
+}
+for (var i = 1; i < mainContainerMb.length; i++) {
+  mainContainerMb[i].style.display = "none";
 }
 
 placement.addEventListener("click", () => {
@@ -21,6 +25,17 @@ placement.addEventListener("click", () => {
   if (!buttons[0].classList.contains("active-button"))
     buttons[0].classList += " active-button";
   mainContainer[0].style.display = "block";
+});
+placement.addEventListener("click", () => {
+  for (var i = 0; i < mainContainerMb.length; i++) {
+    mainContainerMb[i].style.display = "none";
+  }
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active-button");
+  }
+  if (!buttons[0].classList.contains("active-button"))
+    buttons[0].classList += " active-button";
+  mainContainerMb[0].style.display = "block";
 });
 
 facilities.addEventListener("click", () => {
@@ -35,6 +50,18 @@ facilities.addEventListener("click", () => {
   mainContainer[1].style.display = "block";
 });
 
+facilities.addEventListener("click", () => {
+  for (var i = 0; i < mainContainerMb.length; i++) {
+    mainContainerMb[i].style.display = "none";
+  }
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active-button");
+  }
+  if (!facilities.classList.contains("active-button"))
+    facilities.classList += " active-button";
+  mainContainerMb[1].style.display = "block";
+});
+
 demographics.addEventListener("click", () => {
   for (var i = 0; i < mainContainer.length; i++) {
     mainContainer[i].style.display = "none";
@@ -47,6 +74,18 @@ demographics.addEventListener("click", () => {
   mainContainer[2].style.display = "block";
 });
 
+demographics.addEventListener("click", () => {
+  for (var i = 0; i < mainContainerMb.length; i++) {
+    mainContainerMb[i].style.display = "none";
+  }
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active-button");
+  }
+  if (!demographics.classList.contains("active-button"))
+    demographics.classList += " active-button";
+  mainContainerMb[2].style.display = "block";
+});
+
 representatives.addEventListener("click", () => {
   for (var i = 0; i < mainContainer.length; i++) {
     mainContainer[i].style.display = "none";
@@ -57,4 +96,16 @@ representatives.addEventListener("click", () => {
   if (!representatives.classList.contains("active-button"))
     representatives.classList += " active-button";
   mainContainer[3].style.display = "block";
+});
+
+representatives.addEventListener("click", () => {
+  for (var i = 0; i < mainContainerMb.length; i++) {
+    mainContainerMb[i].style.display = "none";
+  }
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active-button");
+  }
+  if (!representatives.classList.contains("active-button"))
+    representatives.classList += " active-button";
+  mainContainerMb[3].style.display = "block";
 });
